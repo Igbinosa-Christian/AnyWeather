3e29815f16b1e339afdc5ef43da27974ef51ba35
Manor <igbinosa62@gmail.com>
2023-02-08 03:23:23 +0000

    First Commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request, flash, redirect, url_for
 import requests
 
 
@@ -14,43 +14,10 @@ app.config['SECRET_KEY']='ddhhyedewdyurwiu4u838'
 def index():
 
     weather_data = []
- 
-    # To display current city weather
-    url_ip = 'https://ipinfo.io/json?token=3da5e5f0b554e7'
-
-    re = requests.get(url_ip).json()
-
-        
-        
-    city_name = re['city']
-
-    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid=04d6a59f7d372177853b85deb5c0379c&units=imperial'
-    city = city_name
-
-
-    r = requests.get(url.format(city)).json()
-
-    if r['cod'] == '404':
-        flash("City Not Found")
-
-    else:
-
-        weather = {
-            'city': city,
-            'country': r['sys']['country'],
-            'temperature': round((r['main']['temp']), 2),
-            'temperature_celcius': round((r['main']['temp']-32) * (5/9), 2),
-            'description': r['weather'][0]['description'],
-            'icon': r['weather'][0]['icon']
-         }
-
-        weather_data.append(weather)
-
 
     # Display serached city weather
     if request.method == 'POST':
-        
-        weather_data.pop(0)
+
         city_name = request.form.get('search_city')
 
         url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid=04d6a59f7d372177853b85deb5c0379c&units=imperial'
